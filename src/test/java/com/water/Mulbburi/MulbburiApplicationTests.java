e4c77f7b37daa4256e7bde181da66f457b71e069
zinndryke7 <zinndryke7@gmail.com>
2023-03-16 10:48:55 +0900

    commit

---
+++ src/test/java/com/water/Mulbburi/MulbburiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water.Mulbburi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulbburiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
