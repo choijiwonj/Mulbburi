f7367b0403a7f47708104180efcdb7db4e4e180d
Young-Ju <kyj198706@gmail.com>
2023-03-17 15:36:41 +0900

    지원한일

---
+++ src/main/java/com/water/Mulbburi/community/controller/CommunityController.java
@@ -0,0 +1,20 @@
+package com.water.Mulbburi.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/community")
+public class CommunityController {
+	
+	
+	@GetMapping("/main")
+	public String main() {
+		return "community/communityMain";
+	}
+
+}
--- src/main/java/com/water/Mulbburi/consumer/controller/ConsumerController.java
+++ src/main/java/com/water/Mulbburi/consumer/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package com.water.Mulbburi.consumer.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,10 +13,41 @@ import lombok.extern.slf4j.Slf4j;
 
 public class ConsumerController {
 	
+	@GetMapping("/main")
+	public String main() {
+		return "consumer/consumerMain";
+	}
+	
+	
+	
 	@GetMapping("/list")
 	public String list() {
 		
-		return "consumer/list";
+		return "consumer/consumerList";
+	}
+	
+	@GetMapping("/detail")
+	public String detail() {
+		
+		return "consumer/consumerDetail";
 	}
 
+	@GetMapping("/write")
+	public String write() {
+		
+		return "consumer/consumerWrite";
+	}
+	@GetMapping("/googi")
+	public String googi() {
+		
+		return "consumer/consumerGoogi";
+	}
+	
+	@GetMapping("/faq")
+	public String faq() {
+		
+		return "consumer/consumerFAQ";
+	}
+	
+	
 }
