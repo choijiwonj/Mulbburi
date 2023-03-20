0d2abb80da5bf4c098756ef482df651db71bc6b9
Young-Ju <kyj198706@gmail.com>
2023-03-20 17:49:12 +0900

    오늘 html 완성,기능 구현중

---
+++ src/main/java/com/water/Mulbburi/community/dao/CommunityMapper.java
@@ -0,0 +1,26 @@
+package com.water.Mulbburi.community.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.water.Mulbburi.common.paging.SelectCriteria;
+import com.water.Mulbburi.community.dto.CommunityDTO;
+
+@Mapper
+public class CommunityMapper {
+
+	public int selectTotalCount(Map<String, String> searchMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public List<CommunityDTO> selectCommunityList(SelectCriteria selectCriteria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
