73153544d20aade33d5e02db2c3337a5a56bffaf
_kimtaehun <rth0204@gmail.com>
2023-03-21 14:07:50 +0900

    깃테스트

--- src/main/java/com/water/Mulbburi/management/service/ManagementService.java
+++ src/main/java/com/water/Mulbburi/management/service/ManagementService.java
@@ -1,12 +1,15 @@
 package com.water.Mulbburi.management.service;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.greedy.comprehensive.common.paging.SelectCriteria;
+import com.water.Mulbburi.common.paging.Pagenation;
+import com.water.Mulbburi.common.paging.SelectCriteria;
 import com.water.Mulbburi.management.dao.ManagementMapper;
+import com.water.Mulbburi.management.dto.ManagementDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,9 +32,14 @@ public class ManagementService {
 		int limit = 1;
 		int buttonAmount = 3;
 		
-//		SelectCriteria selectCriteria =
+		SelectCriteria selectCriteria = Pagenation.getSelectCriteria(page, totalCount, limit, buttonAmount);
+		log.info("[ManagementService] selectCriteria : {}", selectCriteria);
+		
+		List<ManagementDTO> orderAll = managementMapper.checkorderAllList(selectCriteria);
+		log.info("[ManagementService] orderAll : {}", orderAll);
 		
 		return null;
+		
 	}
 	
 }
