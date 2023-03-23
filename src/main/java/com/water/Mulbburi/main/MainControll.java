f04fdfc1cda187b0132adfd16bcbfd146da978af
kim sang yeob <tkdduq717@gmail.com>
2023-03-23 13:58:17 +0900

    상엽 비밀번호찾기 변경 및 구현중

--- src/main/java/com/water/Mulbburi/main/MainControll.java
+++ src/main/java/com/water/Mulbburi/main/MainControll.java
@@ -1,5 +1,19 @@
 package com.water.Mulbburi.main;
 
-public class AskController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/main")
+public class MainControll {
+
+	@GetMapping("/01-1.purchaseLogin")
+	public String main() {
+		
+		return "/main/01-1.purchaseLogin";
+	}
 }
