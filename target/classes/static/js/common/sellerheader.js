4b2d58fa26706cc307e04edc57612325f9c8441e
choijiwonj <126842391+choijiwonj@users.noreply.github.com>
2023-03-27 19:02:55 +0900

    Delete target/classes directory

--- target/classes/static/js/common/sellerheader.js
+++
@@ -1,6 +0,0 @@
-function home(){
-		document.querySelector(".sellerhome").addEventListener("click", () => {location.href="/Mulbburiseller"});
-		document.querySelector(".logout").addEventListener("click", () => {location.href="/Mulbburi"});
-}
-
-home();
