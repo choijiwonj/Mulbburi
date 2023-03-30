671f4c572c5112217bb837bb1becd8f474aa10bb
choijiwonj <126842391+choijiwonj@users.noreply.github.com>
2023-03-30 11:58:35 +0900

    Update FileDTO.java

--- src/main/java/com/water/Mulbburi/file/FileDTO.java
+++ src/main/java/com/water/Mulbburi/file/FileDTO.java
@@ -1,16 +1,13 @@
 package com.water.Mulbburi.file;
 
-import java.util.List;
-
 import lombok.Data;
 
 @Data
 public class FileDTO {
 
 	private int fileNo;
-	private String fileoriginalName;
+	private String fileOriginalName;
 	private String filePath;
-	private String fileSize;
 	private String fileSavedName;
 	private int custom;
 	private int sbNo;
@@ -18,36 +15,4 @@ public class FileDTO {
 	private int communityNo;
 	private int reviewNo;
 	private int bannerNo;
-	private int ytNo;
-	private List<FileDTO> fileList;
-	public void setThumbnailPath(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setOriginalName(String originalFileName) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setSavedName(String savedFileName) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setSavePath(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void setFileType(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public String getSavePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getSavedName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	}
-
+}
