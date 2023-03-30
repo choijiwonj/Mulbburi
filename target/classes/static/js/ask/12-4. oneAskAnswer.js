f7a80b90f1170c13c986ffa906ca7a0947d82070
choijiwonj <126842391+choijiwonj@users.noreply.github.com>
2023-03-30 12:27:37 +0900

    Delete target directory

--- target/classes/static/js/ask/12-4. oneAskAnswer.js
+++
@@ -1,5 +0,0 @@
-function close(){
-    document.querySelector(".answerid").addEventListener("click", () => {self.close()});
-}
-
-close();
--- target/classes/static/js/seller/06-2. sellerAccept.js
+++
@@ -1,9 +0,0 @@
-function click(){
-    const $button3 = document.querySelector(".div2");
-    
-    console.log($button3);
-
-    $button3.addEventListener("click", () => {window.open("/acceptDetail", "width=500, height=500, left=100, top=50")});
-}
-
-click();
