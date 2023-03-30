98789c2256fd7364bdfd009763a403ea7b8ca886
choijiwonj <126842391+choijiwonj@users.noreply.github.com>
2023-03-30 14:35:18 +0900

    Create ProductMapper.java

---
+++ src/main/java/com/water/Mulbburi/product/dao/ProductMapper.java
@@ -0,0 +1,26 @@
+package com.water.Mulbburi.product.dao;
+​
+import java.util.List;
+import java.util.Map;
+​
+import org.apache.ibatis.annotations.Mapper;
+​
+import com.water.Mulbburi.common.paging.SelectCriteria;
+import com.water.Mulbburi.file.FileDTO;
+import com.water.Mulbburi.product.dto.ProductDTO;
+​
+@Mapper
+public interface ProductMapper {
+​
+	
+	  /* 상품 삽입 */ 
+	  void prodRegist(ProductDTO product);
+	  
+	  /* 파일 삽입 */
+	  void insertFile(FileDTO attachment);
+	 
+	  /* 전체 목록 조회 */
+	  int checkProdTotalCount();
+	  List<ProductDTO> checkAllproduct(SelectCriteria selectCriteria);
+​
+}
